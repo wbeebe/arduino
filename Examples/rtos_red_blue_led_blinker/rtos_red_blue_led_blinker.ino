b22bf231cd34eae9bd30aabe4c7ece56b495ab59
Bill Beebe <wbeebe@gmail.com>
2019-11-13 07:04:09 -0500

    Minor updates.

--- Examples/rtos_red_blue_led_blinker/rtos_red_blue_led_blinker.ino
+++ Examples/rtos_red_blue_led_blinker/rtos_red_blue_led_blinker.ino
@@ -14,22 +14,28 @@
 
 #include <Arduino.h>
 
-/*
- * Sketch demonstate mutli-task using Scheduler. Demo creates a loop2() 
- * that runs in 'parallel' with loop().
- * - loop() toggle LED_RED every 1 second
- * - loop2() toggle LED_BLUE every 300 milliseconds
+/**
+ * Sketch demonstate mutli-task using Scheduler.
+ * Demo creates a loop2() that runs in 'parallel' with loop().
+ * - loop() toggles LED_RED every 1 second
+ * - loop2() toggles LED_BLUE every 300 milliseconds
  */
 
 void setup() {
+    // whb Nov 2019
+    // This must be performed first or else nothing else will work.
+    // No, I don't know exactly why.
+    //
     Serial.begin(115200);
-    // LED_RED & LED_BLUE pin are already initialized as an output.
+
+    // LED_RED & LED_BLUE pins are already initialized outputs.
     // Create loop2() using Scheduler to run in 'parallel' with loop()
+    //
     Scheduler.startLoop(loop2);
 }
 
 /**
- * Toggle red LED every second
+ * Toggle the Red LED every second
  */
 void loop() {
     digitalToggle(LED_RED); // Toggle LED 
@@ -37,7 +43,7 @@ void loop() {
 }
 
 /**
- * Toggle blue LED every 300 milliseconds
+ * Toggle the Blue LED every 300 milliseconds
  */
 void loop2() {
     digitalToggle(LED_BLUE); // Toggle LED 
